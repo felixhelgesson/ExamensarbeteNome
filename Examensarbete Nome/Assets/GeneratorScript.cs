06998b240f4d819fb6c0935c7dcac772e75f0051
epain17 <epain17@gmail.com>
2018-04-20 18:53:13 +0200

    Merge branch 'master' into Sounds2

--- Examensarbete Nome/Assets/GeneratorScript.cs
+++ Examensarbete Nome/Assets/GeneratorScript.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class GeneratorScript : MonoBehaviour {
 
     public bool powerON = false;
+    public AudioClip generatorSound;
+    AudioSource aS;
     Animator generatorAnimCtrl;
+    
 
 
 	// Use this for initialization
 	void Start () {
 
         generatorAnimCtrl = GetComponentInParent<Animator>();
+        aS = GetComponent<AudioSource>();
 		
 	}
 
     void OnTriggerEnter(Collider col)
     {
         generatorAnimCtrl.SetBool("StartGenerator", true);
+        aS.PlayOneShot(generatorSound);
         powerON = true;
     }
 
